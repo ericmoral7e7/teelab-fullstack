1aeb95e36ecbc0e9aaa0ae8b39cd7cae5665fa13
Èric <eric.moral.7e7@itb.cat>
2026-04-06 17:02:11 +0200

    Refactor cargarTicket function for improved readability and date formatting

--- frontend/js/ticket.js
+++ frontend/js/ticket.js
@@ -4,18 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
     cargarTicket();
 });
 
-function cargarTicket(){
+function cargarTicket() {
     const infoTicket = getLastTicket()
-    
+
     document.getElementById("ticket-id").innerText = infoTicket.id
-    document.getElementById("ticket-fecha").innerText = infoTicket.fecha
+    document.getElementById("ticket-fecha").innerText = new Date(infoTicket.fecha).toLocaleString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit' });
+
 
     const contenedor = document.getElementById("ticket-body")
 
     infoTicket.items.forEach(camiseta => {
         contenedor.appendChild(crearFilaTabla(camiseta))
     });
-    
+
     document.getElementById("ticket-total").innerText = infoTicket.total + "€";
 }
 
